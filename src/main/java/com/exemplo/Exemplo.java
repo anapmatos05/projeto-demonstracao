1fae61cd3d5c54d3b5dbee291ca840ec37ab1613
Ana Matos <ana.matos@ipvc.pt>
2026-03-11 15:19:13 +0000

    Primeiro Commit

---
+++ src/main/java/com/exemplo/Exemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.exemplo;
+
+/**
+ *
+ * @author anapm
+ */
+public class Exemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
