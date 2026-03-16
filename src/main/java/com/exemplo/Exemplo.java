3b4ddc401fc289d9e689465ac857521c47fbf4c7
Ana Matos <ana.matos@ipvc.pt>
2026-03-16 14:41:43 +0000

    Teste

--- src/main/java/com/exemplo/Exemplo.java
+++ src/main/java/com/exemplo/Exemplo.java
@@ -9,8 +9,8 @@ package com.exemplo;
  * @author anapm
  */
 public class Exemplo {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Teste de Conexão: NetBeans + GitHub");
+        System.out.println("O Push funcionou com sucesso!");
     }
 }
