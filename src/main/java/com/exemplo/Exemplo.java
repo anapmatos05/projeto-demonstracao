6ede8b3e426d5f674090b303d3b4f3aaf5421413
Ana Matos <ana.matos@ipvc.pt>
2026-03-17 14:06:11 +0000

    Teste

--- src/main/java/com/exemplo/Exemplo.java
+++ src/main/java/com/exemplo/Exemplo.java
@@ -8,9 +8,37 @@ package com.exemplo;
  *
  * @author anapm
  */
-public class Exemplo {
+/**public class Exemplo {
     public static void main(String[] args) {
         System.out.println("Teste de Conexão: NetBeans + GitHub");
         System.out.println("O Push funcionou com sucesso!");
+    }
+*/
+
+import java.util.Scanner;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("--- Sistema de Notas do Aluno ---");
+        
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = leitor.nextDouble();
+        
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = leitor.nextDouble();
+        
+        double media = (nota1 + nota2) / 2;
+        
+        System.out.println("A média final é: " + media);
+        
+        if (media >= 10) {
+            System.out.println("Resultado: Aprovado! Parabéns.");
+        } else {
+            System.out.println("Resultado: Reprovado. Precisa de estudar mais.");
+        }
+        
+        System.out.println("---------------------------------");
     }
 }
